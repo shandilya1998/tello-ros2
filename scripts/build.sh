dc38a246d4e0c81c00a14c1586f7b21e8099bb37
shandilya1998 <shandilya.shreyas@gmail.com>
2024-07-09 23:12:00 +0530

    Repository reorganisation for integrating with nrt_ws
    Problem
    =======
    1. Launch files and config files placed in this repo
    2. Redundant nesting in folder structure
    
    Solution
    ========
    1. Shifted Launch files and config files to controllers package
    2. Removed redundtant directory nesting
    
    Note
    ====
    Build Successful

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ../workspace
-rm -rf build install log
-
-rosdep install -i --from-path src
-colcon build --symlink-install --packages-select tello tello_control tello_msg
--- scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Source the workspace
-cd ../workspace
-. install/setup.bash
-
-# Run packages
-cd src
-ros2 launch launch.py
